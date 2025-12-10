94d63050a69fb050d6f7f2fdff6e547c7c57bcf1
Arthur Reis <hartielag@gmail.com>
2025-12-10 13:55:53 +0000

    feat: implement Person and Contact Models with relationships

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'country_code',
+        'number',
+        'person_id';
+    ];
+
+    /**
+     * Relationship: A Contact belongs to a Person.
+     */
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
 }
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Person extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    /**
+     * Relationship: A Person has many Contacts.
+     */
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
